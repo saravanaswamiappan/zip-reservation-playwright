8878671ce34b1ba02825bdc736feb7217cdde226
Saravana Kumar <Saravana.Kumar@theaccessgroup.com>
2025-09-24 11:10:24 +1000

    adjusted dates

--- fixtures/Builders/datesBuilder.js
+++ fixtures/Builders/datesBuilder.js
@@ -2,8 +2,8 @@ import { faker } from '@faker-js/faker';
 import dayjs from 'dayjs';
 
 export function  generateDates() {
-  const checkindate = dayjs().add(11, 'day').format('DD/MM/YYYY');
-  const checkoutdate = dayjs().add(13, 'day').format('DD/MM/YYYY');  
+  const checkindate = dayjs().add(1, 'day').format('DD/MM/YYYY');
+  const checkoutdate = dayjs().add(3, 'day').format('DD/MM/YYYY');  
 
   return {
     checkindate,
